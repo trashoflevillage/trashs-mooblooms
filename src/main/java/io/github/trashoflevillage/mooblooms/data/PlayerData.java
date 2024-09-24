f3c6271c7edfc62d61566478932d29857d79b667
trashoflevillage <uhhnokthxbye@gmail.com>
2024-09-23 17:05:10 -1100

    Suspicious Brewery advancement completed.

---
+++ src/main/java/io/github/trashoflevillage/mooblooms/data/PlayerData.java
@@ -0,0 +1,8 @@
+package io.github.trashoflevillage.mooblooms.data;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class PlayerData {
+    public long lastMooshroomStewMilk = -1;
+}
--- src/main/java/io/github/trashoflevillage/mooblooms/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/trashoflevillage/mooblooms/mixin/PlayerEntityMixin.java
@@ -1,5 +1,7 @@
 package io.github.trashoflevillage.mooblooms.mixin;
 
+import io.github.trashoflevillage.mooblooms.TrashsMooblooms;
+import io.github.trashoflevillage.mooblooms.data.PlayerData;
 import io.github.trashoflevillage.mooblooms.entity.ModEntities;
 import io.github.trashoflevillage.mooblooms.items.ModItems;
 import net.minecraft.advancement.Advancement;
@@ -18,7 +20,9 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.World;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,50 +30,43 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 @Mixin(PlayerEntity.class)
-public class PlayerEntityMixin extends LivingEntity {
-	private long lastMooshroomStewMilk = -1;
+public abstract class PlayerEntityMixin extends LivingEntity {
+	private static HashMap<UUID, PlayerData> playerData = new HashMap<>();
 
 	protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
 
-	@Inject(at = @At("HEAD"), method = "interact")
+	@Inject(method = "interact", at = @At("HEAD"))
 	private void interact(Entity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
 		if (entity.getType() == EntityType.MOOSHROOM && getStackInHand(hand).itemMatches(Items.BOWL.getRegistryEntry())) {
-			lastMooshroomStewMilk = getEntityWorld().getTime();
+			getPlayerData().lastMooshroomStewMilk = getEntityWorld().getTime();
 		}
 
 		if (entity.getType() == ModEntities.MOOBLOOM && getStackInHand(hand).itemMatches(Items.MUSHROOM_STEW.getRegistryEntry())) {
-			if (lastMooshroomStewMilk - getEntityWorld().getTime() < 600) {
+			if (getEntityWorld().getTime() - getPlayerData().lastMooshroomStewMilk < 600 && getPlayerData().lastMooshroomStewMilk != -1) {
 				PlayerEntity player = this.getEntityWorld().getPlayerByUuid(this.uuid);
 				if (player instanceof ServerPlayerEntity) {
-					(ServerPlayerEntity)((ServerPlayerEntity) player)
-							.getAdvancementTracker()
-							.grantCriterion(
-
-							)
+					ServerPlayerEntity serverPlayer = ((ServerPlayerEntity) player);
+					serverPlayer
+						.getAdvancementTracker()
+						.grantCriterion(
+							serverPlayer.server.getAdvancementLoader().get(
+									Identifier.of(TrashsMooblooms.MOD_ID, "suspicious_brewery")
+							), "milk_mooshroom_then_moobloom_for_stew"
+						);
 				}
 			}
 		}
 	}
 
-	@Override
-	public Iterable<ItemStack> getArmorItems() {
-		return null;
-	}
-
-	@Override
-	public ItemStack getEquippedStack(EquipmentSlot slot) {
-		return null;
-	}
-
-	@Override
-	public void equipStack(EquipmentSlot slot, ItemStack stack) {}
-
-	@Override
-	public Arm getMainArm() {
-		return null;
+	public PlayerData getPlayerData() {
+		if (!playerData.containsKey(this.uuid)) playerData.put(this.uuid, new PlayerData());
+		return playerData.get(this.uuid);
 	}
 }

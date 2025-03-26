39fb167c2395142a84f98a47011f73f7ab3816c3
trashoflevillage <uhhnokthxbye@gmail.com>
2025-03-26 11:49:14 -0400

    Dying items works properly-ish not fully

--- common/src/main/java/io/github/trashoflevillage/manymooblooms/ManyMooblooms.java
+++ common/src/main/java/io/github/trashoflevillage/manymooblooms/ManyMooblooms.java
@@ -1,10 +1,16 @@
 package io.github.trashoflevillage.manymooblooms;
 
+import dev.architectury.registry.ReloadListenerRegistry;
 import dev.architectury.registry.level.entity.EntityAttributeRegistry;
 import io.github.trashoflevillage.manymooblooms.block.ModBlocks;
 import io.github.trashoflevillage.manymooblooms.entity.ModEntities;
 import io.github.trashoflevillage.manymooblooms.item.ModItems;
 import net.minecraft.entity.passive.CowEntity;
+import net.minecraft.resource.ResourcePackProfile;
+import net.minecraft.resource.ResourcePackSource;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 public final class ManyMooblooms {
     public static final String MOD_ID = "manymooblooms";
@@ -15,4 +21,5 @@ public final class ManyMooblooms {
         ModItems.registerAll();
         ModEntities.registerAlL();
     }
+
 }
--- neoforge/src/main/java/io/github/trashoflevillage/manymooblooms/neoforge/ManyMoobloomsNeoForge.java
+++ neoforge/src/main/java/io/github/trashoflevillage/manymooblooms/neoforge/ManyMoobloomsNeoForge.java
@@ -1,12 +1,20 @@
 package io.github.trashoflevillage.manymooblooms.neoforge;
 
+import net.minecraft.resource.ResourcePackProfile;
+import net.minecraft.resource.ResourcePackSource;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 import io.github.trashoflevillage.manymooblooms.ManyMooblooms;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
 
 @Mod(ManyMooblooms.MOD_ID)
 public final class ManyMoobloomsNeoForge {
-    public ManyMoobloomsNeoForge() {
+    public ManyMoobloomsNeoForge(IEventBus modBus) {
         // Run our common setup.
         ManyMooblooms.init();
     }
